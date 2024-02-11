4afd0e4a81ede9c92057d1f6a09899d851196c6a
codingVikram <multani_v@hotmail.com>
2024-02-11 10:19:07 +0000

    details api endpoint

--- customers/views.py
+++ customers/views.py
@@ -8,3 +8,10 @@ def customers(request):
     data = Customer.objects.all()
     serializer = CustomerSerializer(data, many=True)
     return JsonResponse({'customers': serializer.data})
+
+
+
+def customer(request,id):
+    data = Customer.objects.get(pk=id)
+    serializer = CustomerSerializer(data)
+    return JsonResponse({'customer': serializer.data})
